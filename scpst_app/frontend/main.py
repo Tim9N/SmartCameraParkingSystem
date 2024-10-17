90808ca427a33d1c6d5ff30506694561c7854f0f
Tim9N <tninan26@gmail.com>
2024-10-17 11:25:08 -0400

    updated main to use bucket image

--- scpst_app/frontend/main.py
+++ scpst_app/frontend/main.py
@@ -1,59 +1,77 @@
 # External imports
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 import requests
+import cv2 as cv
+import subprocess
+from bucketAPI import download_from_bucket
 
-# Internal imports
-import dataReader # To pass info between python files
 
 def main():
-
+    
     # Path to the images
-    imagePathHeader = "static/images/Cars"
+    imagePathHeader = 'static/images/'
+    imageSubPath = 'fromthepi.jpg'
 
     # Counter for the images
     i = 1
     
     # headless chrome
+    service = Service('/usr/bin/chromedriver')
     options = webdriver.ChromeOptions()
     options.add_argument('--headless=new')
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(service=service, options=options)
 
-    # Loop every 5 seconds
     while True:
+
+        
+
         # Get the image path
-        imagePath = imagePathHeader + str(i%13) + ".png"
+        imagePath = imagePathHeader + imageSubPath
 
-        # Write the image path to the data.txt file
-        #dataReader.write_image(imagePath)
+        # Get the image from the bucket
+        download_from_bucket(imageSubPath, imagePath)
 
-        # Send the image path to the backend
-        requests.post('http://127.0.0.1:5000/setImage', json={'image': imagePath})
-
+        requests.post('http://127.0.0.1:5000/setImage', json={'image':imagePath})
+        
         time.sleep(5)
-
-        # Increment the counter 
-        i += 1
-
+        
         # Print the image path
         print("Trying image: " + imagePath)
         
         #selenium load the page 
         driver.get('http://127.0.0.1:5000/getImage')
-
-        # wait for 5 seconds
-        time.sleep(10)
-
-        # Get the coordinates from the backend
-        response = requests.get('http://127.0.0.1:5000/getCoordinates')
+        
+        while True:
+            response = requests.get('http://127.0.0.1:5000/getCounter')
+            counter_value = response.json().get('val')
+            print(f'We got {counter_value} from the server')
+            if counter_value == i:
+                break
+            else:
+                time.sleep(5)
+        i += 1
+        
+        response = requests.get('http://127.0.0.1:5000/getData')
+    
         data = response.json()
-        print(data)
-        print(data['top'])
-
-        # # Output the top and left coordinates of the license plate
-        # print("Top: " + str(dataReader.get_top()))
-        # print("Left: " + str(dataReader.get_left()))
-
+        top = data.get('top')
+        if top == None:
+            print('Nothing Detected')
+            continue
+        left = data.get('left')
+        width = data.get('boxWidth')
+        height = data.get('boxHeight')
+        
+        print('Saving cropped image')
+        im = cv.imread(imagePath)
+        im = im[int(top):int(top+height),int(left):int(left+width)]
+        cv.imwrite('cropped.jpg', im)
+        
+        print(top, left, width, height)
+        print('Calling OCR')
+        subprocess.run(['python', 'ocr.py'])
 
 if __name__ == "__main__":
     main()
